9c1c540cf8ca1097f0dccdb5dd8d135c17174032
uyoo <hs27951@gmail.com>
2019-04-04 21:50:55 +0900

    solve week_07 problem(9202, 4358)

--- week_07/uyoo/TreeDistribution_4358.java
+++ week_07/uyoo/TreeDistribution_4358.java
@@ -1,8 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.io.*;
 
 class Trie_4358 {
     Node_4358 root;
@@ -11,13 +7,11 @@ class Trie_4358 {
         this.root = new Node_4358(' ');
     }
 
-    public void insert(String input){
-        String str = input.toUpperCase().replace(" ", "");
-
+    public void insert(String str){
         Node_4358 current = root;
         for(int i=0; i<str.length(); i++){
             char data = str.charAt(i);
-            int index = str.charAt(i) - 'A';
+            int index = str.charAt(i) - 0;
 
             if(current.child[index] == null){
                 current.child[index] = new Node_4358(data);
@@ -31,14 +25,15 @@ class Trie_4358 {
 
         } else {
             current.isFinished = true;
-            current.word = input;
+            current.word = str;
         }
     }
 
-    public void getAllLeaf(Node_4358 node, double totalSpeciesCnt){
+    //DFS -> 사전에 존재하는 문자를 탐색해 문자 발견 시 비율 출력
+    public void getAllLeaf(Node_4358 node, int totalSpeciesCnt){
         if(node.isFinished) {
             String word = node.word;
-            int wordCnt = node.wordCnt;
+            double wordCnt = node.wordCnt;
             double ratio = (wordCnt / totalSpeciesCnt) * 100;
 
             System.out.print(word + " ");
@@ -63,35 +58,25 @@ class Node_4358 {
 
     public Node_4358(char data) {
         this.data = data;
-        this.child = new Node_4358[26];
+        this.child = new Node_4358[128];
         isFinished = false;
     }
 }
 
 public class TreeDistribution_4358 {
-    static double totalSpeciesCnt = 0.0d;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Scanner scanner = new Scanner(System.in);
 
         Trie_4358 trie = new Trie_4358();
         String input;
-        while (true){
-            input = br.readLine();
-            if(input.equals("")) break;
-
-            //좀 더 정확하게는 중복되는 종이 있으면 총 count를 늘리면 x..
-//            if(trie.insert(input)){
-//               totalSpeciesCnt += 1;
-//            }
-
+        int totalSpeciesCnt = 0;
+        while ((input = br.readLine()) != null && input.length() != 0){
             trie.insert(input);
             totalSpeciesCnt += 1;
         }
 
         //최종 단어들(leaf)을 탐색해 각 단어가 갖는 개수로 백분율 계산
         trie.getAllLeaf(trie.root, totalSpeciesCnt);
-
     }
 }
