51f356b552ddca586bdaadb4da2493836c5c44ba
WONILLISM <wonillism@gmail.com>
2019-04-27 18:11:55 +0900

    Solve BOJ_Step02 (4calculating)

---
+++ BOJ_Step/Step2/04_Division(1008).cpp
@@ -0,0 +1,7 @@
+#include<cstdio>
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%.10lf", a / (double)b);
+	return 0;
+}
---
+++ BOJ_Step/Step2/05_4calculating(10869).cpp
@@ -0,0 +1,12 @@
+#include<cstdio>
+
+int main() {
+	unsigned int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", a + b);
+	printf("%d\n", a - b);
+	printf("%d\n", a * b);
+	printf("%d\n", a / b);
+	printf("%d\n", a % b);
+	return 0;
+}
