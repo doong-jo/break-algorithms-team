eb36e19f3a3719165fe87535ba6aa4e16d1f4e4f
uyoo <hs27951@gmail.com>
2019-05-02 18:16:18 +0900

    delete master files

--- BOJ_Step/Step1/06_We love kriii(10718).cpp
+++
@@ -1,6 +0,0 @@
-#include<cstdio>
-int main() {
-	printf("강한친구 대한육군\n");
-	printf("강한친구 대한육군\n");
-	return 0;
-}
--- BOJ_Step/Step2/04_Division(1008).cpp
+++
@@ -1,7 +0,0 @@
-#include<cstdio>
-int main() {
-	int a, b;
-	scanf("%d %d", &a, &b);
-	printf("%.10lf", a / (double)b);
-	return 0;
-}
